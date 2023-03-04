6ab230b8d2b79a8b1cb1b23264520479c3ff9254
Arian <arian.agha8002@gmail.com>
2023-03-04 22:24:00 +0000

    Decryption functionality added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 import numpy as np
 
 def changeHexToDenary(table):
@@ -17,9 +18,22 @@ def inverseTable(table):
         inversedTable[table[i]] = i
     return inversedTable
 
-def createKeySchedules(key, boxSize, rounds):
+def createKeySchedules(key, boxSize, rounds, encrypt=True, inverse_permutation_table=None):
     portion = len(key) - rounds*boxSize
-    return [int(key[i:i+portion], 2) for i in range(0, (rounds+1)*boxSize, boxSize)]
+    keySchedules = [int(key[i:i+portion], 2) for i in range(0, (rounds+1)*boxSize, boxSize)]
+    if (encrypt):
+        return keySchedules
+    else:
+        if (inverse_permutation_table[0] == None): return -1
+        return reverseAndPermutateKeys(keySchedules, inverse_permutation_table, portion)
+
+def reverseAndPermutateKeys(keySchedules, inverse_permutation_table, portion):
+    newKeySchedules = copy.deepcopy(keySchedules)
+    for i in range(1, len(keySchedules)-1):
+        newKeySchedules[i] = int(permutate(format(keySchedules[i],
+                                                  f'0{portion}b'), inverse_permutation_table), 2)
+    newKeySchedules.reverse()
+    return newKeySchedules
 
 def keyMix(input, keySchedules, round):
     return input ^ keySchedules[round-1]
@@ -38,8 +52,9 @@ def permutate(plaintext, permutation_table):
         ciphertext[permutation_table[i]] = plaintext[i]
     return "".join(ciphertext)
 
-def encrypt(plaintext, key, substitution_table, boxSize, permutation_table, rounds):
-    keySchedules = createKeySchedules(key, boxSize, rounds)
+def encrypt(plaintext, key, substitution_table, boxSize, permutation_table,
+            rounds, encrypt=True):
+    keySchedules = createKeySchedules(key, boxSize, rounds, encrypt, permutation_table)
     plaintext_length = len(plaintext)
     for i in range(1, rounds): # r=1 to Nr-1
         plaintext = format((keyMix(int(plaintext, 2), keySchedules, i)),
@@ -75,14 +90,11 @@ def setupProcess():
     ciphertext = encrypt(plaintext, key, substitution_table, 4, permutation_table, 4)
     print(ciphertext)
 
-    # plaintext = '1011001001000011'
-    # keySchedules = createKeySchedules(key, 4, 4)
-    # key = permutate('0111011001111001', inverseTable(permutation_table)) # Key 2
-    # keySchedules[1] = int(key, 2)
-    # plaintext = permutate(plaintext, inverseTable(permutation_table))
-    # plaintext = format((keyMix(int(plaintext, 2), keySchedules, 2)),
-    #                     f'0{len(plaintext)}b')
-    # print(plaintext)
+    inverse_permutation_table = inverseTable(permutation_table)
+    inverse_substitution_table = inverseTable(substitution_table)
+    plaintext = encrypt(ciphertext, key, inverse_substitution_table, 4,
+                        inverse_permutation_table, 4, False)
+    print(plaintext)
 
 # Main program
 setupProcess()
